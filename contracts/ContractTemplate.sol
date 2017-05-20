74a4830687d52faba94648f29fd2cda79411a012
Miha Eleršič <miha.elersic@gmail.com>
2017-05-20 22:49:38 +0200

    initial

---
+++ contracts/ContractTemplate.sol
@@ -0,0 +1,75 @@
+pragma solidity ^0.4.6;
+contract ContractTemplate {
+  // state
+  address mediator;
+  address proposer;
+  uint quota;
+  uint bettingOn;
+  uint created;
+  mapping(address => uint) otherBets;
+  address[] otherAddrs;
+  bool canceled;
+  uint private betPool;
+  string betName;
+  //constructor
+  function ContractTemplate(string betName_, uint bettingOn_, 
+                            uint quota_, address mediator_) 
+                            payable {
+    mediator = mediator_;
+    betName = betName_;
+    bettingOn = bettingOn_;
+    quota = quota_;
+    created = now;
+    canceled = false;
+    betPool = msg.value;
+    proposer = msg.sender;
+  }
+  //modifiers
+  modifier onlyMediator {
+    if(msg.sender != mediator){
+      throw;
+    }
+    _;
+  }
+  modifier onlyProposer {
+    if (msg.sender != proposer){
+      throw;
+    }
+    _;
+  }
+  modifier active {
+    if(canceled){
+      throw;
+    }  
+    _;
+  }
+
+
+  function bet() payable active{
+    address newGuy = msg.sender;
+    uint betSize = msg.value * quota / 1000;
+    if(betSize<=0 || betSize>betPool){
+      throw;
+    }
+    betPool -= betSize;
+    if (otherBets[newGuy]==0){
+      otherAddrs.push(newGuy);
+    }
+    otherBets[newGuy] += betSize;
+  }
+  function cancel() onlyProposer { 
+    canceled = true;
+  }
+
+  function mediate(uint result) onlyMediator {
+    if (result!=bettingOn){
+      //Izplacaj theOtherGuys
+      for (uint i = 0; i < otherAddrs.length; i++){
+        if (!otherAddrs[i].send(otherBets[otherAddrs[i]])){
+          throw;
+        }
+      }
+    }
+    selfdestruct(proposer);
+  }
+}
